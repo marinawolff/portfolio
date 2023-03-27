aa7ee21e3448c2538ea1485af2c98894c9e6347a
Marina Wolff <wolf0107@algonquinlive.com>
2023-03-27 01:09:45 -0400

    fixes responsiveness

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 const nav = document.querySelector(".nav");
+const burguer = document.querySelector('.burguer');
+const navMobile = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.navbar li');
 
 
 let lastScrollY = window.scrollY;
@@ -12,3 +15,22 @@ window.addEventListener("scroll", () => {
 
   lastScrollY = window.scrollY;
 });
+
+const navSlide = () => {
+  burguer.addEventListener('click', () => {
+    navMobile.classList.toggle('nav-active');
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      }
+       else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+       }
+    });
+  });
+
+
+}
+
+navSlide();
